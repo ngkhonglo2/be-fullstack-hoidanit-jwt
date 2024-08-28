753afefbbdda34600ae650a54f4d3f776886011e
phongnt <bongdem30101998@gmail.com>
2024-08-28 20:27:58 +0700

    feat(add module): add modules and config file main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ReviewsModule } from './modules/reviews/reviews.module';
+import { RestaurantsModule } from './modules/restaurants/restaurants.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { OrderDetailModule } from './modules/order.detail/order.detail.module';
+import { MenusModule } from './modules/menus/menus.module';
+import { MenuItemsModule } from './modules/menu.items/menu.items.module';
+import { MenuItemOptionsModule } from './modules/menu.item.options/menu.item.options.module';
+import { LikesModule } from './modules/likes/likes.module';
 
 @Module({
   imports: [
@@ -18,6 +26,14 @@ import { MongooseModule } from '@nestjs/mongoose';
       }),
       inject: [ConfigService],
     }),
+    ReviewsModule,
+    RestaurantsModule,
+    OrdersModule,
+    OrderDetailModule,
+    MenusModule,
+    MenuItemsModule,
+    MenuItemOptionsModule,
+    LikesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
